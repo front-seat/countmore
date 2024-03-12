c7705d52cfe9aeda55ba7e77bae32feda42b725e
Dave Peck <davepeck@gmail.com>
2024-03-12 12:18:03 -0700

    Add CountMore-branded banner to voter registration page. (#2)
    * Who built the site
    
    * Branded banner on voter registration form.
    
    * Maybe some updates
    
    * remove 'below'

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,8 @@ export default {
   theme: {
     screens: {
       sm: '376px',
+      // The VoteAmerica breakpoint is 664px
+      va: '664px',
       md: '768px',
       lg: '960px',
       xl: '1280px',
