0ec24ca6408193d0dd0964c86976b228fcb68aea
Dave Peck <davepeck@gmail.com>
2024-03-12 14:11:43 -0700

    Basic FAQ folding/unfolding (#3)
    * Clean up FAQ section a little
    
    * FAQ unfold

--- src/components/MoreAndText.tsx
+++ src/components/MoreAndText.tsx
@@ -2,78 +2,130 @@ import { useState } from "react";
 import { type RegistrationHandler } from "../utils/analytics";
 import { More } from "./More";
 
+const QA: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <div>{children}</div>
+);
+
+const Question: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <p className="font-cabinet font-extrabold text-[24px]">{children}</p>
+);
+
+const Answer: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <div className="font-satoshi font-medium text-[20px] leading-[30px] pt-2 space-y-4">
+    {children}
+  </div>
+);
+
+const FrequentlyAskedQuestions: React.FC = () => (
+  <div className="md:border-4 mt-4 md:m-4 border-transparent p-1 md:p-4 space-y-8">
+    {/**-----------------------------------------------*/}
+    <QA>
+      <Question>Why does my vote count more in certain states?</Question>
+      <Answer>
+        <p>
+          Your vote counts more in a swing state because it can swing the
+          election.
+        </p>
+        <p>
+          Swing states are states where the election is close and a small number
+          of votes can change the outcome. For example, in 2020, Biden won the
+          state of Georgia by a razor-thin margin of just 11,779 votes — and
+          there are more than 300,000 college students in Georgia!
+        </p>
+      </Answer>
+    </QA>
+    {/**-----------------------------------------------*/}
+    <QA>
+      <Question>How do you decide which states count more?</Question>
+      <Answer>
+        <p>
+          Our list of swing states is from the{" "}
+          <a
+            href="https://www.cookpolitical.com/ratings/presidential-race-ratings"
+            target="_blank"
+            className="underline cursor-pointer md:hover:text-gray-600 transition-colors duration-200"
+          >
+            2024 Cook Political Report
+          </a>
+          .
+        </p>
+      </Answer>
+    </QA>
+    {/**-----------------------------------------------*/}
+    <QA>
+      <Question>
+        Can I legally register in my home state or school state?
+      </Question>
+      <Answer>
+        <p>
+          Yes, you can register in either state. Registering to vote in a new
+          state will typically cancel your registration in any previous state.
+          Voting in two states is illegal, being registered in two states is
+          not.
+        </p>
+      </Answer>
+    </QA>
+    {/**-----------------------------------------------*/}
+    <QA>
+      <Question>Who built this site?</Question>
+      <Answer>
+        <p>
+          Hi. We're{" "}
+          <a
+            href="https://frontseat.org/"
+            target="_blank"
+            className="underline cursor-pointer md:hover:text-gray-600 transition-colors duration-200"
+          >
+            Front Seat
+          </a>
+          , a group of civic tech builders based in sunny Seattle, Washington.
+          We built this site in partnership with{" "}
+          <a
+            href="https://voteamerica.com/"
+            target="_blank"
+            className="underline cursor-pointer md:hover:text-gray-600 transition-colors duration-200"
+          >
+            VoteAmerica
+          </a>
+          , a trusted name in online voter registration.
+        </p>
+      </Answer>
+    </QA>
+  </div>
+);
+
+const FoldableFAQ: React.FC<{
+  folded: boolean;
+  onUnfold: () => void;
+}> = ({ folded, onUnfold }) => {
+  // When folded, simply show the first <Question> as a link that can be
+  // clicked to unfold the FAQ. Otherwise, show the full FAQ.
+  if (folded) {
+    return (
+      <div className="md:border-4 mt-4 md:m-4 border-transparent p-1 md:p-4 space-y-8">
+        <p
+          className="font-cabinet font-extrabold text-[24px] cursor-pointer underline"
+          onClick={onUnfold}
+        >
+          Frequently asked questions
+        </p>
+      </div>
+    );
+  }
+  return <FrequentlyAskedQuestions />;
+};
+
 const MoreAndText: React.FC<{ handler: RegistrationHandler }> = ({
   handler,
 }) => {
-  const [showText, setShowText] = useState(true);
+  const [showFAQ, setShowFAQ] = useState(true);
 
   return (
     <>
       <div className="border-4 mt-4 md:m-4 border-black p-4">
-        <More handler={handler} onSelectStates={() => setShowText(false)} />
+        <More handler={handler} onSelectStates={() => setShowFAQ(false)} />
       </div>
-      {showText && (
-        <div className="md:border-4 mt-4 md:m-4 border-transparent p-1 md:p-4">
-          <p className="font-cabinet font-extrabold text-[24px]">
-            Why does my vote count more in certain states?
-          </p>
-          <p className="font-satoshi font-medium text-[20px] leading-[30px] pt-2">
-            Your vote counts more in a swing state because it can swing the
-            election.
-          </p>
-          <p className="font-satoshi font-medium text-[20px] leading-[30px] pt-4">
-            Swing states are states where the election is close and a small
-            number of votes can change the outcome. For example, in 2020, Biden
-            won the state of Georgia by a razor-thin margin of just 11,779 votes
-            — and there are more than 300,000 college students in Georgia!
-          </p>
-          <p className="font-cabinet font-extrabold text-[24px] pt-8">
-            How do you decide which states count more?
-          </p>
-          <p className="font-satoshi font-medium text-[20px] leading-[30px] pt-2">
-            Our list of swing states is from the{" "}
-            <a
-              href="https://www.cookpolitical.com/ratings/presidential-race-ratings"
-              target="_blank"
-              className="underline cursor-pointer md:hover:text-gray-600 transition-colors duration-200"
-            >
-              2024 Cook Political Report
-            </a>
-            .
-          </p>
-          <p className="font-cabinet font-extrabold text-[24px] pt-8">
-            Can I legally register in my home state or school state?
-          </p>
-          <p className="font-satoshi font-medium text-[20px] leading-[30px] pt-2">
-            Yes, you can register in either state. Registering to vote in a new
-            state will typically cancel your registration in any previous state.
-            Voting in two states is illegal, being registered in two states is not.
-          </p>
-          <p className="font-cabinet font-extrabold text-[24px] pt-8">
-            Who built this site?
-          </p>
-          <p className="font-satoshi font-medium text-[20px] leading-[30px] pt-2">
-            Hi. We're{" "}
-            <a
-              href="https://frontseat.org/"
-              target="_blank"
-              className="underline cursor-pointer md:hover:text-gray-600 transition-colors duration-200"
-            >
-              Front Seat
-            </a>
-            , a group of civic tech builders based in sunny Seattle, Washington.
-            We built this site in partnership with{" "}
-            <a
-              href="https://voteamerica.com/"
-              target="_blank"
-              className="underline cursor-pointer md:hover:text-gray-600 transition-colors duration-200"
-            >
-              VoteAmerica
-            </a>
-            , a trusted name in online voter registration.
-          </p>
-        </div>
-      )}
+      <FoldableFAQ folded={!showFAQ} onUnfold={() => setShowFAQ(true)} />
     </>
   );
 };
